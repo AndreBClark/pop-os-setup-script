e343b4fd6c3745aee92f4d6ace4f06d52984c66c
Andre <andre@cosmicdivision.dev>
2021-08-04 16:30:40 -0700

    set alacritty as default terminal application

--- apps/alacritty.sh
+++ apps/alacritty.sh
@@ -3,3 +3,7 @@
 ./functions/runner.sh "alacritty" "sudo apt-get install -y alacritty"
 # copy the config file
 ./alacritty.yml > "$HOME"/.alacritty.yml
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 50
+
+sudo update-alternatives --auto x-terminal-emulator
