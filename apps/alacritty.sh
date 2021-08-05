e57a0e14542ccef92c742ef881525d4e59190ab6
Andre <andre@cosmicdivision.dev>
2021-08-04 18:09:19 -0700

    move alacritty config to config folder

--- apps/alacritty.sh
+++ apps/alacritty.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 ./functions/runner.sh "alacritty" "sudo apt-get install -y alacritty"
-# copy the config file
-./alacritty.yml > "$HOME"/.alacritty.yml
+
+# ./config/alacritty.yml > "$HOME"/.alacritty.yml
+chmod +x "$HOME"/.alacritty.yml
+# copy ./config/alacritty.yml to "$HOME"/alacritty.yml
+cp ./config/alacritty.yml "$HOME"/.alacritty.yml
+
+
 
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 50
 
