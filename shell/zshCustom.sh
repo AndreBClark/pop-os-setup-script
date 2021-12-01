33be03838b6629136e0b0fa784675233aec36e51
AndreBClark <andre@cosmicdivision.dev>
2021-11-30 19:27:36 -0800

    move zsh config files to config folder

--- shell/zshCustom.sh
+++ shell/zshCustom.sh
@@ -7,10 +7,12 @@ scripts=(
   "$(sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended)"
 
   #install zsh config file from gist
-  "$(curl -fsSL https://gist.githubusercontent.com/AndreBClark/41e7d1249dc690e7fe3ee0ed3b9215e1/raw/c3e7302cf61359a93b6bedac76016fa78e5391d6/.zshrc -o "/home/$USER/.zshrc")"
+  cp ./config/zshrc ~/.zshrc
+  # "$(curl -fsSL https://gist.githubusercontent.com/AndreBClark/41e7d1249dc690e7fe3ee0ed3b9215e1/raw/c3e7302cf61359a93b6bedac76016fa78e5391d6/.zshrc -o "/home/$USER/.zshrc")"
   "setupnord"
   #download shades-of-nord.zsh-theme from gist
-  "$(curl -fsSL https://gist.githubusercontent.com/AndreBClark/7f9ec73f0a1d4beb8f9c5b2e8be0e8ba/raw/877d21187f655a52cb47243f25b6c0ff30abf2f5/shades-of-nord.zsh-theme -o "$HOME/.oh-my-zsh/themes/shades-of-nord.zsh-theme")"
+  cp ./config/shades-of-nord.zsh-theme ~/.oh-my-zsh/themes/shades-of-nord.zsh-theme
+  # "$(curl -fsSL https://gist.githubusercontent.com/AndreBClark/7f9ec73f0a1d4beb8f9c5b2e8be0e8ba/raw/877d21187f655a52cb47243f25b6c0ff30abf2f5/shades-of-nord.zsh-theme -o "$HOME/.oh-my-zsh/themes/shades-of-nord.zsh-theme")"
   "$(
     # if oh-my-zsh is installed
     if [ -d "$HOME/.oh-my-zsh" ]; then
